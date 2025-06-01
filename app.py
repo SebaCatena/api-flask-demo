4d062cc03d94ed181cc9a6bf0f3e5617f6a30b11
Sebastian <catenaccisebastian@gmail.com>
2025-05-31 21:38:33 -0300

    Proyecto con Flask y API externa

---
+++ app.py
@@ -0,0 +1,13 @@
+from flask import Flask, render_template
+import requests
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    response = requests.get('https://jsonplaceholder.typicode.com/posts')
+    posts = response.json()
+    return render_template('posts.html', posts=posts[:5])
+
+if __name__ == '__main__':
+    app.run(debug=True)
